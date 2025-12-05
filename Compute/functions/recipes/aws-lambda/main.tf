09743bc5011aea461eeab37c1c85678622afb3ab
willdavsmith <willdavsmith@gmail.com>
2025-12-05 00:18:41 -0800

    WIP
    Signed-off-by: willdavsmith <willdavsmith@gmail.com>

--- Compute/functions/recipes/aws-lambda/main.tf
+++ Compute/functions/recipes/aws-lambda/main.tf
@@ -106,8 +106,8 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = var.context.properties.access_key
-  secret_key = var.context.properties.secret_key
+  access_key = var.context.access_key
+  secret_key = var.context.secret_key
 }
 
 # IAM role for the Lambda function with basic execution permissions.
@@ -149,7 +149,7 @@ resource "aws_lambda_function" "container" {
   function_name = var.function_name
   role          = aws_iam_role.lambda_exec.arn
   package_type  = "Image"
-  image_uri     = var.context.properties.image
+  image_uri     = var.context.image
 
   architectures = var.architectures
   timeout       = var.timeout
